9085cf68adf3902bbe3d761bd1087ba9c89901eb
Peanut <p.nuss96@outlook.de>
2022-01-30 14:57:28 +0100

    TextPopUp

---
+++ KatzenMoegenDieGGJ2022/Assets/Scripts/UI/TextPopUp.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextPopUp : MonoBehaviour
+{
+    public GameObject textObject;
+    public float time;
+    public LeanTweenType type;
+    void Start()
+    {
+        textObject.transform.localScale = new Vector3(0, 1, 1);
+    }
+
+
+
+    public void OnPopUp()
+    {
+        textObject.SetActive(true);
+        LeanTween.cancel(textObject);
+        LeanTween.scaleX(textObject, 1, time).setEase(type);
+    }
+
+    public void OnPopAway()
+    {
+        textObject.SetActive(false);
+        LeanTween.cancel(textObject);
+        LeanTween.scaleX(textObject, 0, time).setEase(type);
+    }
+}
